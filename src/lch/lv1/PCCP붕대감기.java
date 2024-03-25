f7533830b6d5f942d24c876d2d1ad649303bcce3
zack8361 <zack6353@naver.com>
2024-03-26 00:26:29 +0900

    붕대감기 75점 / 가장많이 받은 선물
    붕대감기 75점 / 가장많이 받은 선물

--- src/lch/lv1/PCCP붕대감기.java
+++ src/lch/lv1/PCCP붕대감기.java
@@ -1,5 +1,11 @@
 package lch.lv1;
 
+import java.util.HashMap;
+
+
+/**
+ * (70/100)점
+ */
 public class PCCP붕대감기 {
     public static void main(String[] args) {
         int[] bandage = {5,1,5};
@@ -11,23 +17,51 @@ public class PCCP붕대감기 {
     private static int solution(int[] bandage, int health, int[][] attacks) {
         int fit = health;
         int answer =0;
-        int max = -1;
+        int maxTime = -1;
+        for (int i = 0; i < attacks.length; i++) {
+            maxTime = Math.max(maxTime,attacks[i][0]);
+        }
+
+        // 기술 시전 시간
+        int time = bandage[0];
+
+        // 초당 회복량
+        int recovery_amount = bandage[1];
+
+        // 추가 회복량
+        int add_amount = bandage[2];
+
+        // 연속 성공량
+        int cnt = 0;
+
+
+        HashMap<Integer,Integer> attackMap = new HashMap<>();
 
         for (int i = 0; i < attacks.length; i++) {
-            max = Math.max(max,attacks[i][0]);
+            attackMap.put(attacks[i][0], attacks[i][1]);
         }
 
+        for (int i = 1; i <= maxTime; i++) {
+            // 연속 성공량이 5 되면 0으로 초기화
+            if (cnt == time) {
+                fit += add_amount;
+                cnt = 0;
+            }
+            //
+            if(fit >= health) {
+                fit = health;
+            }
 
-        for (int i = 0; i < max; i++) {
-            if(fit>=health){
-                continue;
+            if(attackMap.containsKey(i)){
+                fit -= attackMap.get(i);
+                if (fit < 0) return -1; // 캐릭터 사망
             }
             else {
-                
+                cnt++;
+                fit += recovery_amount;
             }
         }
 
-
-        return answer;
+        return (fit <= 0) ? -1 : fit;
     }
 }
---
+++ src/lch/lv1/가장많이받은선물.java
@@ -0,0 +1,7 @@
+package lch.lv1;
+
+public class 가장많이받은선물 {
+    public static void main(String[] args) {
+        
+    }
+}
