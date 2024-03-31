803c11c07fea05f07b56a917c7d71bf8891ed655
2Smean <136636643+2Smean@users.noreply.github.com>
2024-04-01 01:03:56 +0900

    commit 20240401

---
+++ src/lsm/level0/Lv0_60_배열에서문자열대소문자변환하기.java
@@ -0,0 +1,25 @@
+package lsm.level0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lv0_60_배열에서문자열대소문자변환하기 {
+    public static void main(String[] args) {
+        
+    }
+    public String[] solution(String[] strArr) {
+        List<String> answerList = new ArrayList<>();
+
+        for(int i=0;i<strArr.length;i++){
+            if(strArr.length % 2 == 1){//홀수일때
+                answerList.add(String.valueOf(Character.toUpperCase(Integer.parseInt(strArr[i]))));
+            }else{
+                answerList.add(String.valueOf(Character.toLowerCase(Integer.parseInt(strArr[i]))));
+            }
+        }
+
+        String[] answer = new String[answerList.size()];
+        answer = answerList.toArray(answer);
+        return answer;
+    }
+}
