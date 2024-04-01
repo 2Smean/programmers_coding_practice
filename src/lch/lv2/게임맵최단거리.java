8c4b1a584198912fdf52bd50412f816fe65c4eff
zack8361 <71763096+zack8361@users.noreply.github.com>
2024-04-01 16:55:42 +0900

    bfs 기초 문제 - 게임맵최단거리
    bfs 기초 문제 - 게임맵최단거리

--- src/lch/lv2/게임맵최단거리.java
+++ src/lch/lv2/게임맵최단거리.java
@@ -4,6 +4,9 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 
 public class 게임맵최단거리 {
+
+    private static int[][] copyMap;
+    private static boolean[][] visitied;
     private static int[] dx = {1,0,-1,0};
     private static int[] dy = {0,1,0,-1};
     public static void main(String[] args) {
@@ -28,17 +31,34 @@ public class 게임맵최단거리 {
 
     private static int solution(int[][] maps) {
 
+        copyMap = maps;
+        visitied = new boolean[maps.length][maps[0].length];
 
         ArrayDeque<Node> deque = new ArrayDeque<>();
         deque.addLast(new Node(0,0));
-        int[][] map = new int[maps.length][maps[0].length];
+        visitied[0][0] = true;
 
+        while (!deque.isEmpty()){
+            Node now = deque.pollFirst();
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
 
-        for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps.length; j++) {
-                
+                if(nx<0 || ny<0 || nx>= maps.length || ny >= maps.length){
+                    continue;
+                }
+                if(visitied[nx][ny]){
+                    continue;
+                }
+                if(!visitied[nx][ny] && copyMap[nx][ny] != 0) {
+                    visitied[nx][ny] = true;
+                    copyMap[nx][ny] = copyMap[now.x][now.y] + 1;
+                    deque.addLast(new Node(nx, ny));
+                }
             }
         }
-        return 0;
+
+        int ans = copyMap[maps.length-1][maps[0].length-1];
+        return (visitied[maps.length-1][maps[0].length-1]) ? ans : -1 ;
     }
 }
