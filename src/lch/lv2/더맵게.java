885f172c90f0ca9c3ac0a7530626a9ada978d34f
zack8361 <71763096+zack8361@users.noreply.github.com>
2024-04-02 17:56:35 +0900

    더맵게 런타임에러 잡기 아마 NULLPOINTER
    더맵게 런타임에러 잡기 아마 NULLPOINTER

--- src/lch/lv2/더맵게.java
+++ src/lch/lv2/더맵게.java
@@ -1,7 +1,34 @@
 package lch.lv2;
 
+import java.util.PriorityQueue;
+
 public class 더맵게 {
     public static void main(String[] args) {
-        
+
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int k = 7;
+        System.out.println(solution(scoville,k));
+    }
+
+    private static int solution(int[] scoville, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int scoop : scoville) {
+            pq.add(scoop);
+        }
+
+        int ans = 0;
+
+        while (true){
+            if(pq.peek() >= k) {
+                return ans;
+            }
+            else {
+                int firstNum = pq.poll();
+                int secondNum = pq.poll();
+                pq.add(firstNum +(secondNum * 2));
+                ans++;
+            }
+        }
+
     }
 }
