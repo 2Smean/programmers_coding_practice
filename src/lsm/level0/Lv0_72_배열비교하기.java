5ece76f79f9f55730ec36200a35bd436a02523ca
2Smean <136636643+2Smean@users.noreply.github.com>
2024-04-05 18:43:28 +0900

    commit 20240405 풀이변경

--- src/lsm/level0/Lv0_72_배열비교하기.java
+++ src/lsm/level0/Lv0_72_배열비교하기.java
@@ -1,13 +1,6 @@
 package lsm.level0;
-
-public class Lv0_72_배열비교하기 {
-    public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {1, 2, 3, 4, 5};
-
-        System.out.println(solution(arr1,arr2));
-    }
-    private static int solution(int[] arr1, int[] arr2) {
+/*테스트코드 추가로 인한 새로운 풀이 ( 삼항연산자 사용한 풀이로 변경 )
+* private static int solution(int[] arr1, int[] arr2) {
         int answer = 0;
         int arr1result = 0;
         int arr2result = 0;
@@ -28,6 +21,34 @@ public class Lv0_72_배열비교하기 {
             }
         }
         return answer;
+    }*/
+public class Lv0_72_배열비교하기 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 2, 3, 4, 5};
+
+        System.out.println(solution(arr1,arr2));
+    }
+    private static int solution(int[] arr1, int[] arr2) {
+        int arr1result = 0;
+        int arr2result = 0;
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr1result += arr1[i];
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            arr2result += arr2[i];
+        }
+
+        if(arr1.length != arr2.length){
+            return arr1.length > arr2.length ? 1 : -1;
+        }else{
+            if(arr1result == arr2result){
+                return 0;
+            }
+            return arr1result > arr2result ? 1: -1;
+        }
     }
 
 }
