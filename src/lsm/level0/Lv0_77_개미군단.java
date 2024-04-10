d1fdf60813703aad37f869b99d12623a7f04412d
2Smean <136636643+2Smean@users.noreply.github.com>
2024-04-10 14:01:27 +0900

    commit 20240410 Release

--- src/lsm/level0/Lv0_77_개미군단.java
+++ src/lsm/level0/Lv0_77_개미군단.java
@@ -2,7 +2,7 @@ package lsm.level0;
 
 public class Lv0_77_개미군단 {
     public static void main(String[] args) {
-        int hp = 21;
+        int hp = 24;
         System.out.println(solution(hp));
     }
     private static int solution(int hp) {
@@ -10,16 +10,16 @@ public class Lv0_77_개미군단 {
         int soldierAnt = 3;
         int workerAnt = 1;
 
-        int generalsNeeded = hp / generalAnt;
-        int remainingHP = hp % generalAnt;
+        int generalsNeeded = hp / generalAnt; //generalNeeded 는 4
+        int remainingHP = hp % generalAnt; //remainingHP 는 3
 
-        if (remainingHP >= soldierAnt) {
-            generalsNeeded++;
-            remainingHP -= soldierAnt;
+        if (remainingHP >= soldierAnt) {//우선 soldierAnt 를 걸러서 수를 센다.
+            generalsNeeded++; //위의 경우가 참이라면 generalsNeeded 에 1을 추가하고
+            remainingHP -= soldierAnt; //추가되었으므로 기존의 나머지에서 soldierAnt 의 값을 뺀다
         }
 
-        if (remainingHP >= workerAnt) {
-            generalsNeeded += remainingHP;
+        if (remainingHP >= workerAnt) { //remainingHp 에는 1의 값이 남아있으므로 해당 조건문 참
+            generalsNeeded += remainingHP; // 3이하의 나머지는 몫에 더해주면 된다
         }
 
         return generalsNeeded;
