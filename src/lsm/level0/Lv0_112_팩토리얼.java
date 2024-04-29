882d32960f118d025b27e1ee2ee786b2c78b6180
2Smean <136636643+2Smean@users.noreply.github.com>
2024-04-29 16:55:57 +0900

    commit 20240429

--- src/lsm/level0/Lv0_112_팩토리얼.java
+++ src/lsm/level0/Lv0_112_팩토리얼.java
@@ -2,20 +2,24 @@ package lsm.level0;
 
 public class Lv0_112_팩토리얼 {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 3628800;
         System.out.println(solution(n));
     }
     private static int solution(int n) {
+        int answer = 0;
         int factorial = 1;
-        int i = 1;
 
-        // 팩토리얼 값을 계산하여 n보다 작거나 같은 경우까지 반복
-        while (factorial <= n) {
-            i++; // i를 1씩 증가시키면서
-            factorial *= i; // 팩토리얼 값을 갱신
+        for(int i=1;i<=10;i++){
+            factorial = factorial * i;
+
+            if(factorial == n){
+                answer = i;
+                break;
+            }else if(n < factorial){
+                answer = (i-1);
+                break;
+            }
         }
-
-        // 이때의 i가 조건을 만족하는 가장 큰 정수임
-        return i - 1;
+        return answer;
     }
 }
