d7ca6c4ed919531d1295a2fded52ffd9c826e491
2Smean <136636643+2Smean@users.noreply.github.com>
2024-05-09 18:54:56 +0900

    commit 20240509

--- src/lsm/level0/Lv0_114_A로B만들기.java
+++ src/lsm/level0/Lv0_114_A로B만들기.java
@@ -9,11 +9,17 @@ public class Lv0_114_A로B만들기 {
         System.out.println(solution(be,af));
     }
     private static int solution(String before, String after) {
+        int answer = 0;
         char[] beforeChars = before.toCharArray();
         char[] afterChars = after.toCharArray();
+
         Arrays.sort(beforeChars);
         Arrays.sort(afterChars);
 
-        return Arrays.equals(beforeChars, afterChars) ? 1 : 0;
+        if(Arrays.equals(beforeChars, afterChars)){
+            answer = 1;
+        }
+
+        return answer;
     }
 }
