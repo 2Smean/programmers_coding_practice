8b41d6ee3bacf8353fe7b9b0b80c26f2d63ac603
2Smean <136636643+2Smean@users.noreply.github.com>
2024-07-16 23:01:05 +0900

    commit 20240716

---
+++ src/lsm/level0/Lv0_133_a와b출력하기.java
@@ -0,0 +1,15 @@
+package lsm.level0;
+
+import java.util.Scanner;
+
+public class Lv0_133_a와b출력하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
