5bbdda0b47b6b57bec9c7cac41e2b2e1664587c7
2Smean <136636643+2Smean@users.noreply.github.com>
2024-07-21 22:56:43 +0900

    commit 20240721

---
+++ src/lsm/level0/Lv0_139_PCCE기출문제1번_출력.java
@@ -0,0 +1,14 @@
+package lsm.level0;
+
+public class Lv0_139_PCCE기출문제1번_출력 {
+
+    public static void main(String[] args) {
+        String msg = "spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
